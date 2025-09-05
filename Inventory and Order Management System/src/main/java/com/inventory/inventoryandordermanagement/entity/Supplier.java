193e2b2ee49399580625260b9db96bf22c79c22a
Parthib Saha <105928504+parthibCsaha@users.noreply.github.com>
2025-09-05 14:06:31 +0600

    Add files via upload

---
+++ Inventory and Order Management System/src/main/java/com/inventory/inventoryandordermanagement/entity/Supplier.java
@@ -0,0 +1,35 @@
+package com.inventory.inventoryandordermanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String email;
+    private String phone;
+    private String address;
+
+    @OneToMany(mappedBy = "supplier")
+    private List<Product> products;
+
+    @Version
+    private Integer version;
+
+}
