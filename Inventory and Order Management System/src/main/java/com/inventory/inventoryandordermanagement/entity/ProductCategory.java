95797542c55579b881f7c68eb2f5bd1dba2601d9
Parthib Saha <105928504+parthibCsaha@users.noreply.github.com>
2025-09-06 12:36:56 +0600

    Add files via upload

---
+++ Inventory and Order Management System/src/main/java/com/inventory/inventoryandordermanagement/entity/ProductCategory.java
@@ -0,0 +1,34 @@
+package com.inventory.inventoryandordermanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "product_categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "product_category_mapping",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    @Version
+    private Long version;
+
+}
