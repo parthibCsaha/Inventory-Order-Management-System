6529e3916e53100c876c0d5c4790cbe36b889606
Parthib Saha <105928504+parthibCsaha@users.noreply.github.com>
2025-09-11 14:39:12 +0600

    Add files via upload

---
+++ Inventory and Order Management System/src/main/java/com/inventory/inventoryandordermanagement/mapper/ProductMapper.java
@@ -0,0 +1,19 @@
+package com.inventory.inventoryandordermanagement.mapper;
+
+import com.inventory.inventoryandordermanagement.dto.ProductRequest;
+import com.inventory.inventoryandordermanagement.dto.ProductResponse;
+import com.inventory.inventoryandordermanagement.entity.Product;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    Product toEntity(ProductRequest dto);
+    ProductResponse toResponse(Product entity);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(ProductRequest dto, @MappingTarget Product entity);
+
+}
