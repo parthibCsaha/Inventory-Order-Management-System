a887b895d42f1ba56a2fbb70088b23c7c03310d1
Parthib Saha <105928504+parthibCsaha@users.noreply.github.com>
2025-09-24 12:18:55 +0600

    Delete Inventory and Order Management System/src/main/java/com/inventory/inventoryandordermanagement/ResourceNotFoundException.java

--- Inventory and Order Management System/src/main/java/com/inventory/inventoryandordermanagement/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.inventory.inventoryandordermanagement.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
