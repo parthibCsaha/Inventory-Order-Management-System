d667e4e65134f3c681447db34ae6b7ebe8f5bd79
Parthib Saha <105928504+parthibCsaha@users.noreply.github.com>
2025-09-24 12:18:13 +0600

    Delete Inventory and Order Management System/src/main/java/com/inventory/inventoryandordermanagement/ForbiddenException.java

--- Inventory and Order Management System/src/main/java/com/inventory/inventoryandordermanagement/ForbiddenException.java
+++
@@ -1,7 +0,0 @@
-package com.inventory.inventoryandordermanagement.exception;
-
-public class ForbiddenException extends RuntimeException {
-    public ForbiddenException(String message) {
-        super(message);
-    }
-}
