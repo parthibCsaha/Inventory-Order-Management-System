2dba3ea250050a15e41e11ac890e022d7eee013d
Parthib Saha <105928504+parthibCsaha@users.noreply.github.com>
2025-09-24 12:19:49 +0600

    Add files via upload

---
+++ Inventory and Order Management System/src/main/java/com/inventory/inventoryandordermanagement/exception/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.inventory.inventoryandordermanagement.exception;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
